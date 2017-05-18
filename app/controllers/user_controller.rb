1aa9bafcbb4ac0d13723b34f5d399b771da0de4a
Ian Gonnella & Jared Graham & Mendel Dinh & Ryan Austin <pair+iagon.jaredgraham12.mdinh7.ryana760@devbootcamp.com>
2017-05-18 11:43:42 -0700

    Updated things after merge much better now

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,7 +7,7 @@ post '/users' do
   @user = User.create(params[:user])
   if @user.save
     session[:id] = @user.id
-    redirect "/users/#{@user.id}/profile"
+    redirect "/users/#{@user.id}"
   else
     @errors = ['You cannot leave a field blank']
     erb :'/users/new'
@@ -19,18 +19,27 @@ post '/users' do
  end
 
  post '/users/login' do
-   @user = User.find_by(params[:user][:username])
+   @user = User.find_by(username: params[:user][:username])
    if @user.authenticate(params[:user][:password])
      session[:id] = @user.id
-     redirect "/users/#{@user.id}/profile"
+     redirect "/users/#{@user.id}"
    else
      @errors = ['Username or Password did not match']
      erb :'/users/login'
    end
  end
 
-
- get '/users/logout' do
-   session.delete(:id)
+get '/users/logout' do
+   session.clear
    redirect "/"
  end
+
+get '/users/:id' do
+  erb :'users/login' unless session[:id]
+  @user = User.find(params[:id])
+  # @questions = Question.where(params[:id])
+  erb :'users/profile'
+end
+
+
+
