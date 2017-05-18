107cdb32fc300ce814ada25380142fef956fbaf0
Ian Gonnella <iangonnella@gmail.com>
2017-05-18 11:13:43 -0700

    Basic functions for categories

---
+++ app/controllers/index.rb
@@ -0,0 +1,11 @@
+get '/' do
+  @categories = Category.all
+ erb :'/categories/index'
+end
+
+
+get '/category/:id' do
+  @category = Category.find_by(id: params[:id])
+  erb :'/categories/show'
+end
+
