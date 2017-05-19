1f3f79ef1123e668d0bca51ceba70551a960b888
ryana760 <smokinsunrise@gmail.com>
2017-05-19 11:08:52 -0700

    Update profile function working

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -49,13 +49,13 @@ end
 
 
 #edit profile get
-get 'users/:id/edit' do 
+get '/users/:id/edit' do 
   @user = User.find(params[:id])
   erb :'users/update_user'
 end
 
 #update profile post
-put 'users/:id' do 
+put '/users/:id' do 
   @user = User.find(params[:id])
   @user.update_attributes(params[:user])
   @user.password
